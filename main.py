b995b0db63119396cb51c9f7b5bca6359af14d1c
Manoram <manoramsubedi7@gmail.com>
2024-09-25 12:47:56 +0545

    plotting income & expenses in graph

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import csv
 from datetime import datetime
 from data_entry import get_amount, get_category, get_date, get_description
 
+import matplotlib.pyplot as plt
+
 
 class CSV:
     CSV_FILE = "finance_data.csv"
@@ -73,12 +75,30 @@ def add():
     CSV.add_entry(date, amount, category, description)
 
 
+# PLOTTING THE TRANSACTIONS
+def plot_transactions(df):
+    df.set_index('date', inplace=True)
+
+    income_df = df[df['category'] == "Income"].resample("D").sum().reindex(df.index, fill_value=0)
+    expense_df = df[df['category'] == "Expense"].resample("D").sum().reindex(df.index, fill_value=0)
+
+    # PLOTTING
+    plt.figure(figsize=(10,5))
+    plt.plot(income_df.index, income_df["amount"], label="Income", color="g")
+    plt.plot(expense_df.index, expense_df["amount"], label="Expense", color="r")
+    plt.xlabel("Date")
+    plt.ylabel("Income and Expenses Over Time")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
 def main():
     while True:
         print("\n1. Add a new transaction.")
         print("2. View transaction summary.")
         print("3. Exit")
-        choice = input("Enter your choice (!-3): ")
+        choice = input("Enter your choice (1-3): ")
 
         if choice == "1":
             add()
@@ -86,6 +106,9 @@ def main():
             start_date = get_date("Enter the start date (dd-mm-yyyy): ")
             end_date = get_date("Enter the end date (dd-mm-yyyy): ")
             df = CSV.get_transaction(start_date, end_date)
+            if input("Do you want to see a plot? (y/n)").lower() == "y":
+                plot_transactions(df)
+
         elif choice == "3":
             print("Exiting....")
             break
